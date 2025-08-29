70e1a0c7cd2fb06eb8d857e6483f030d2e840aed
Nandhana <nandhanav262@gmail.com>
2025-08-29 15:30:35 +0530

    4 th commit

--- src/test/java/eventhex/PartnersSpotlight.java
+++ src/test/java/eventhex/PartnersSpotlight.java
@@ -3,6 +3,7 @@ package eventhex;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -44,16 +45,37 @@ public class PartnersSpotlight {
             // PartnersSpotlight
             driver.get("https://app.eventhex.ai/event/68abf3eec54d5f0247378640/insta-snap/partners-spotlights");
             
-           
-        
-        
-	
-            
+            WebElement addBtn = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[contains(.,'Add New')]")));
+            ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", addBtn);
+            addBtn.click();
+
+         // Enter Partner Name
+            WebElement partnerName = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@placeholder='Partner Name']"))
+            );
+            partnerName.sendKeys("Test Partner Pvt Ltd");
+
+            // Upload Logo
+            WebElement logoUpload = driver.findElement(By.xpath("//input[@type='file']"));
+            logoUpload.sendKeys("C:\\Users\\USER\\Downloads\\download.jpeg");
+
+            // Enter Partner Link
+            WebElement partnerLink = driver.findElement(By.xpath("//input[@placeholder='Link']"));
+            partnerLink.sendKeys("https://www.testpartner.com");
+
+            //Click Submit
+            WebElement submitBtn = wait.until(
+                ExpectedConditions.elementToBeClickable(By.xpath("//button[normalize-space()='Submit']"))
+            );
+            submitBtn.click();
+
+            System.out.println("Partner Spotlight added successfully!");
+
             
         } catch (Exception e) {
             e.printStackTrace();
-            System.out.println("Error occurred before arranging watermark!");
-        } finally {
+            System.out.println("Error occurred in Partners Spotlight automation!");
+            } finally {
             //driver.quit();
         }
     }
